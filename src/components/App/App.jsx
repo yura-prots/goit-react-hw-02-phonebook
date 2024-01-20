cf1fd21256ab8a359714f9b88914f9b43649a2a0
yura-prots <yuraprots@gmail.com>
2024-01-20 19:23:14 +0200

    add isDuplicate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,12 +10,19 @@ import initialState from 'db/contacts.json';
 class App extends Component {
   state = {
     contacts: initialState,
-    name: '',
-    phone: '',
     filter: '',
   };
 
   addContact = newContact => {
+    const isDuplicate = this.state.contacts.find(
+      contact => contact.name === newContact.name
+    );
+
+    if (isDuplicate) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    }
+
     this.setState(prevState => {
       return {
         contacts: [
@@ -36,7 +43,7 @@ class App extends Component {
   render() {
     const { contacts, filter } = this.state;
 
-    const visibleContactsList = contacts.filter(contact => {
+    const visibleContacts = contacts.filter(contact => {
       return contact.name.toLowerCase().includes(filter.toLowerCase());
     });
 
@@ -46,11 +53,8 @@ class App extends Component {
         <ContactsForm onAdd={this.addContact} />
 
         <h2>Contacts</h2>
-
-        <p>Find contact by name</p>
-        <ContactsFilter filter={filter} toFind={this.findContact} />
-
-        <ContactsList contacts={visibleContactsList} />
+        <ContactsFilter toFind={this.findContact} />
+        <ContactsList contacts={visibleContacts} />
       </div>
     );
   }
--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,7 +1,16 @@
-const ContactsFilter = ({ filter, toFind }) => {
-  //   console.log(filter);
-  //   console.log(toFind);
-  return <input type="text" onChange={e => toFind(e.target.value)} />;
+import PropTypes from 'prop-types';
+
+const ContactsFilter = ({ toFind }) => {
+  return (
+    <div>
+      <p>Find contact by name</p>
+      <input type="text" onChange={e => toFind(e.target.value)} />;
+    </div>
+  );
+};
+
+ContactsFilter.propTypes = {
+  toFind: PropTypes.func.isRequired,
 };
 
 export default ContactsFilter;
